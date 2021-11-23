b9ce6f72822b4118031bcb060a746c7d8744d904
g3teg <g3teg@DESKTOP-SIIESEU>
2021-11-23 10:19:05 +0300

    Добавьте файлы проекта.

---
+++ Lib_11/Diffirence.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibArray;
+
+namespace Lib_11
+{
+
+    public static class Diffirence
+    {
+        public static string DiffirenceOfMas(this MyArray myarray)
+        {
+            int diff = myarray[0];
+
+            for (int i = 1; i < myarray.Length; i++)
+            {
+                if (myarray[i] > 0 || myarray[i] < 0)
+                    diff -= myarray[i];
+            }
+            return diff.ToString();
+        }
+    }
+}
---
+++ Practice_2G/MainWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using LibArray;
+using Lib_11;
+
+namespace Practice_2G
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private MyArray _myArray = new MyArray(5);
+
+        private void DataGrid_CellEditEnding(object sender, DataGridCellEditEndingEventArgs e)
+        {
+            int indexCoulumn = e.Column.DisplayIndex;
+            _myArray[indexCoulumn] = int.Parse(((TextBox)e.EditingElement).Text);
+        }
+
+        public void Calculation_Click(object sender, RoutedEventArgs e)
+        {
+            diff.Clear();
+            diff.Text = _myArray.DiffirenceOfMas();
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Information_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Дроздов Г. ИСП-34", "Разработчик", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void FillPositiveArray_Click(object sender, RoutedEventArgs e)
+        {
+            diff.Clear();
+            _myArray.FillPositiveNumbers();
+            dataGrid.ItemsSource = _myArray.ToDataTable().DefaultView;
+        }
+        private void FillNegativeArray_Click(object sender, RoutedEventArgs e)
+        {
+            diff.Clear();
+            _myArray.FillNegativeNumbers();
+            dataGrid.ItemsSource = _myArray.ToDataTable().DefaultView;
+        }
+
+        private void ClearArray_Click(object sender, RoutedEventArgs e)
+        {
+            _myArray.Clear();
+            diff.Clear();
+            dataGrid.ItemsSource = _myArray.ToDataTable().DefaultView;
+
+        }
+        private void OpenArray_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Текстовый документ (*.txt)|*.txt|Все файлы (*.*)|*.*";
+            openFileDialog.DefaultExt = ".txt";
+            openFileDialog.FilterIndex = 1;
+            openFileDialog.Title = "Импорт массива";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                _myArray.Import(openFileDialog.FileName);
+            }
+            diff.Clear();
+            dataGrid.ItemsSource = _myArray.ToDataTable().DefaultView;
+        }
+
+        private void SaveArray_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Текстовый документ (*.txt)|*.txt|Все файлы (*.*)|*.*";
+            saveFileDialog.DefaultExt = ".txt";
+            saveFileDialog.FilterIndex = 1;
+            saveFileDialog.Title = "Экспорт массива";
+
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                _myArray.Export(saveFileDialog.FileName);
+            }
+            diff.Clear();
+            dataGrid.ItemsSource = null;
+        }
+    }
+}
